64e7f21947477a24b934ba57af225a369eab9615
mineugene <mineugene@users.noreply.github.com>
2022-02-21 19:09:42 -0500

    add snippet plugin for lsp completion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -62,13 +62,14 @@ return require("packer").startup({
       end,
       requires = {
         "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "saadparwaiz1/cmp_luasnip",
-        "petertriho/cmp-git",
         "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lsp-signature-help",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-path",
+        "petertriho/cmp-git",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
       },
     })
 
--- lua/post/cmp.lua
+++ lua/post/cmp.lua
@@ -12,6 +12,11 @@ local cmp = require("cmp")
 local setup_params = {
   settings = {
     enabled = true,
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end
+    },
     mapping = {
       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
