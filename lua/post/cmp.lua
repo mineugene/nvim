552ec8c88ee04a84e931c5d4c5cb104426628928
mineugene <mineugene@users.noreply.github.com>
2022-04-14 22:21:17 -0400

    update hrsh7th/cmp post configuration
    Ref https://github.com/hrsh7th/nvim-cmp/issues/231#issuecomment-1098175017

--- lua/post/cmp.lua
+++ lua/post/cmp.lua
@@ -11,16 +11,13 @@ local settings = {
       require("luasnip").lsp_expand(args.body)
     end,
   },
-  mapping = {
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-e>"] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  },
+  }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "luasnip" },
