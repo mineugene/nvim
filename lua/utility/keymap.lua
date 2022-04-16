4730b4e6824ecfeeb4d058ca76aa3a445eeaec4a
mineugene <mineugene@users.noreply.github.com>
2022-04-15 23:23:50 -0400

    update keymap utility
    Support for lua function as rhs

--- lua/utility/keymap.lua
+++ lua/utility/keymap.lua
@@ -1,9 +1,14 @@
 return {
   setter = function(bufnr, mode, lhs, rhs, opts)
-    if bufnr then
-      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-    else
+    if type(rhs) == "function" then
+      opts.callback = rhs
+      rhs = vim.api.nvim_replace_termcodes("<Nop>", false, false, true)
+    end
+
+    if bufnr == nil then
       vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+    else
+      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
     end
   end,
 }
