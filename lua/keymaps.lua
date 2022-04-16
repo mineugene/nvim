68b019c0835bd90217445f5c2ab333b5d51c3819
mineugene <mineugene@users.noreply.github.com>
2022-04-16 01:01:07 -0400

    update start_terminal keymap
    - Update with the split of nvim_create_buf_term in 461d979
    - Change mapping from <Leader><Bslash> to <C-Bslash><Bslash>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,8 +6,17 @@
 --]]
 
 local DEFAULT_OPTS = { noremap = true, silent = true }
+local util = require("utility")
+
+vim.g.mapleader = " "
+
+local function start_terminal()
+  local bufnr = util.api.nvim_create_buf()
+  util.api.nvim_resize_win()
+  util.api.nvim_open_term(bufnr)
+end
 local nkeymaps = {
-  ["<Leader><Bslash>"] = require("utility").api.nvim_create_buf_term,
+  ["<C-Bslash><Bslash>"] = start_terminal,
   ["<Leader>h"] = "<Cmd>nohlsearch<CR>",
   -- telescope
   ["<Leader>gd"] = "<Cmd>Telescope lsp_definitions<CR>",
@@ -20,10 +29,6 @@ local nkeymaps = {
   -- context-commentstring
   ["<Leader>gc"] = "<Cmd>lua require('ts_context_commentstring.internal').update_commentstring()<CR>",
 }
-
-vim.g.mapleader = " "
-
-local util = require("utility")
 for lhs, rhs in pairs(nkeymaps) do
   util.keymap.set(util.mapmode.NORMAL, lhs, rhs, DEFAULT_OPTS)
 end
