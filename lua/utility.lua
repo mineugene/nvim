aa977a5dcbc2eadcea5e0251cb1f164f7dd7f4f1
mineugene <mineugene@users.noreply.github.com>
2022-04-21 22:50:06 -0400

    replace keymap setter with vim.keymap.set

--- lua/utility.lua
+++ lua/utility.lua
@@ -133,11 +133,7 @@ Utility.keymap = {
   ---@param rhs string|function
   ---@param opts any
   set = function(mode, lhs, rhs, opts)
-    local bufnr = opts.buffer == true and 0 or opts.buffer
-    opts = vim.deepcopy(opts)
-    opts.buffer = nil
-
-    require("utility.keymap").setter(bufnr, mode, lhs, rhs, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
   end,
 }
 
